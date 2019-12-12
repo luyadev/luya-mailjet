206a4369b35ccae38263e847bda39f5c387b8594
Basil <git@nadar.io>
2019-12-12 17:10:30 +0100

    add test

--- tests/models/TemplateTest.php
+++ tests/models/TemplateTest.php
@@ -2,7 +2,9 @@
 
 namespace luya\mailjet\tests\models;
 
+use luya\mailjet\admin\aws\MjmlPreviewActiveWindow;
 use luya\mailjet\models\Template;
+use luya\mailjet\models\TemplateVariable;
 use luya\mailjet\tests\MailjetTestCase;
 use luya\testsuite\fixtures\NgRestModelFixture;
 
@@ -12,6 +14,10 @@ class TemplateTest extends MailjetTestCase
     {
         $fixture = new NgRestModelFixture([
             'modelClass' => Template::class,
+        ]);
+
+        $varFixture = new NgRestModelFixture([
+            'modelClass' => TemplateVariable::class,
         ]);
 
         $model = $fixture->newModel;
@@ -24,5 +30,17 @@ class TemplateTest extends MailjetTestCase
 
         $model->html = '<p></p>';
         $this->assertSame('<p></p>', $model->render());
+
+        // test active window context
+
+        $model->off($model::EVENT_BEFORE_INSERT);
+        $r = $model->save();
+
+
+        $aw = new MjmlPreviewActiveWindow();
+        $aw->ngRestModelClass = get_class($model);
+        $aw->setItemId(1);
+
+        $this->assertNotNUll($aw->index());
     }
 }
