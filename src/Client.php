46854b0bfb59cf5c1ca81fe5133a08fe8bbd1dc7
Basil <git@nadar.io>
2019-12-17 17:59:26 +0100

    add sms handler to luya mailjet client

--- src/Client.php
+++ src/Client.php
@@ -5,13 +5,12 @@ namespace luya\mailjet;
 use yii\base\Component;
 use Mailjet\Client as MailjetClient;
 use yii\base\InvalidConfigException;
-use Mailjet\Resources;
-use luya\helpers\Html;
 
 /**
  * Mailjet Component.
  *
  * @property \Mailjet\Client $client
+ * 
  * @author Basil Suter <basil@nadar.io>
  * @since 1.0.0
  */
@@ -42,7 +41,8 @@ class Client extends Component
     }
     
     /**
-     *
+     * Mailjet PHP SDK Client Library.
+     * 
      * @return \Mailjet\Client
      */
     public function getClient()
@@ -55,8 +55,9 @@ class Client extends Component
     }
     
     /**
-     *
-     * @return \luya\mailjet\Contacts
+     * Mailjet Contacts Handler.
+     * 
+     * @return Contacts
      */
     public function contacts()
     {
@@ -64,11 +65,23 @@ class Client extends Component
     }
     
     /**
-     *
-     * @return \luya\mailjet\Sections
+     * Mailjet Sections Handler.
+     * 
+     * @return Sections
      */
     public function sections()
     {
         return new Sections($this->client);
+    }
+
+    /**
+     * SMS Handler
+     *
+     * @return Sms
+     * @since 1.3.0
+     */
+    public function sms()
+    {
+        return new Sms($this->client);
     }
 }
