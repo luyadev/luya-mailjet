2cca5d17e5c2c0714896e0c6e6b75d07974716a8
Basil Suter <git@nadar.io>
2019-12-17 12:22:22 +0100

    add basic concept

--- src/widgets/SubscribeFormWidget.php
+++ src/widgets/SubscribeFormWidget.php
@@ -5,14 +5,53 @@ namespace luya\mailjet\widgets;
 use Yii;
 use luya\base\DynamicModel;
 use luya\base\Widget;
+use luya\helpers\Json;
+use luya\helpers\Url;
+use luya\mailjet\Contacts;
+use yii\base\InvalidConfigException;
 
+/**
+ * Subscribe Form Widget.
+ * 
+ * ```php
+ * <?php $widget = SubscribeFormWidget::begin(['confirmTemplateId' => 123123, 'hashSecret' => '_ADD_A_RANDOM_STRING_', 'listId' => 123123]) ?>
+ *     <?php if ($widget->isSent): ?>
+ *         <div class="alert alert-success">A confirmation email has been sent to your address. Check your mails.</div>
+ *     <?php elseif ($widget->isConfirmed): ?>
+ *         <div class="alert alert-success">Thanks, your email address has been added to the subscription list.</div>
+ *     <?php else: ?>
+ *         <?php $form = ActiveForm::begin(); ?>
+ *              <?= $form->field($widget->model, 'email'); ?>
+ *              <?= Html::submitButton('Submit'); ?>
+ *         <?php $form::end(); ?>
+ *     <?php endif; ?>
+ * <?php $widget::end(); ?>
+ * ```
+ * 
+ * @property DynamicModel $model
+ * @property string $modelEmail
+ * @property boolean $isSent
+ * @property boolean $isConfirmed
+ */
 class SubscribeFormWidget extends Widget
 {
-    public $hashSecret = 'foobar';
+    const MAIL_SENT_SUCCESS = 'mailSentSuccess';
+    const MAIL_CONFIRM_SUCCESS = 'mailConfirmSuccess';
 
-    public $doubleOptIn = true;
+    public $listId;
 
     public $confirmTemplateId;
+
+    public $hashSecret;
+
+    
+
+    public $listAction = Contacts::ACTION_ADDNOFORCE;
+
+    /**
+     * @var string The name of attribute which contains the email adresse. This attribute email value will be taken to confirm and subscribe
+     */
+    public $emailAttributeName = 'email';
 
     public $attributes = ['email'];
 
@@ -24,30 +63,108 @@ class SubscribeFormWidget extends Widget
     /**
      * @var string An optional route where the confirmation success message should be redirected to after success double opt int.
      */
-    public $confirmPage;
+    //public $confirmPage;
 
-    public function run()
+    /**
+     * @var boolean Whether confirmation mail should be sent or not.
+     */
+    //public $doubleOptIn = true;
+
+    public function init()
     {
-        $model = new DynamicModel($this->attributes);
-        foreach ($this->attributeRules as $rule) {
-            $model->addRule($rule[0], $rule[1]);
+        parent::init();
+
+        if (empty($this->listId) || empty($this->confirmTemplateId) || empty($this->hashSecret)) {
+            throw new InvalidConfigException("The listId, confirmTempalteId and hashSecret properties can not be empty.");
         }
 
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+        $this->processConfirmLink();
+
+        if ($this->getModel()->load(Yii::$app->request->post()) && $this->getModel()->validate()) {
             $keys = [];
-            foreach ($model->attributes as $key => $value) {
+            foreach ($this->getModel()->attributes as $key => $value) {
                 $keys[] = "{$key}:{$value}";
             }
 
-            $crypt = base64_encode(Yii::$app->security->encryptByPassword(implode(",", $keys), $this->hashSecret));
+            $crypt = base64_encode(Yii::$app->security->encryptByPassword(Json::encode($this->getModel()->attributes), $this->hashSecret));
+            $url = Url::appendQuery(['w' => self::getId(), 'subscribe' => $crypt], true);
 
-            var_dump($crypt);
-            exit;
+            if ($this->sendConfirmMail($url, $this->model->attributes)) {
+                Yii::$app->session->setFlash(self::MAIL_SENT_SUCCESS);
+            }
         }
 
-        return $this->render('subscribe-form', [
-            'model' => $model,
-            'attributes' => $this->attributes,
-        ]);
+        ob_start();
+    }
+
+    public function run()
+    {
+        $content = ob_get_clean();
+
+        return $content;
+    }
+
+    private $_model;
+
+    public function getModel()
+    {
+        if ($this->_model === null) {
+            $this->_model = new DynamicModel($this->attributes);
+            foreach ($this->attributeRules as $rule) {
+                $this->_model->addRule($rule[0], $rule[1]);
+            }
+        }
+
+        return $this->_model;
+    }
+
+    public function getModelEmail()
+    {
+        return $this->getModel()->{$this->emailAttributeName};
+    }
+
+    public function getIsSent()
+    {
+        return Yii::$app->session->getFlash(self::MAIL_SENT_SUCCESS);
+    }
+
+    public function getIsConfirmed()
+    {
+        return Yii::$app->session->getFlash(self::MAIL_CONFIRM_SUCCESS);
+    }
+
+    private function sendConfirmMail($url, array $variables = [])
+    {
+        return Yii::$app->mailer->compose()
+            ->setTemplate($this->confirmTemplateId)
+            ->setVariables(array_merge(['url' => $url], $variables))
+            ->setTo($this->getModelEmail())
+            ->send();
+    }
+
+    public function processConfirmLink()
+    {
+        $widgetId = Yii::$app->request->get('w');
+        $subscribe = Yii::$app->request->get('subscribe');
+
+        if ($widgetId == self::getId() && !empty($subscribe)) {
+            $data = Yii::$app->security->decryptByPassword(base64_decode($subscribe), $this->hashSecret);
+            $attributes = Json::decode($data);
+            $this->getModel()->attributes = $attributes;
+            if ($this->getModel()->validate()) {
+                $email = $this->getModelEmail();
+                if ($this->addToList($email, $this->model->attributes)) {
+                    Yii::$app->session->setFlash(self::MAIL_CONFIRM_SUCCESS);
+                }
+            }
+        }
+    }
+
+    public function addToList($email, array $properties = [])
+    {
+        return Yii::$app->mailjet->contacts()
+            ->list($this->listId, $this->listAction)
+                ->add($email, $properties)
+                ->sync();
     }
 }
