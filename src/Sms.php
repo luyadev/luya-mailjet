61ce12612ad82581a5ea0382b70f6c55e60680dc
Basil Suter <git@nadar.io>
2019-12-18 15:59:49 +0100

    add send validation

--- src/Sms.php
+++ src/Sms.php
@@ -2,9 +2,13 @@
 
 namespace luya\mailjet;
 
+use libphonenumber\PhoneNumberType;
+use luya\base\DynamicModel;
+use luya\validators\PhoneNumberValidator;
 use Mailjet\Client;
 use Mailjet\Resources;
 use yii\base\BaseObject;
+use yii\base\InvalidParamException;
 
 /**
  * Send Mailjet SMS
@@ -28,13 +32,31 @@ class Sms extends BaseObject
     /**
      * Send a text message to a given number
      *
+     * We recommend to use {{luya\validators\PhoneNumberValidator}} in order to validate the $to sms recipients number.
+     * 
      * @param string $text The text part of the message.
      * @param string $to Message recipient. Should be between 3 and 15 characters in length. The number always starts with a plus sign followed by a country code, followed by the number. Phone numbers are expected to comply with the E.164 format.
      * @param string $from Customizable sender name. Should be between 3 and 11 characters in length, only alphanumeric characters are allowed.
+     * @param integer Whether the input data should be validated.
+     * @param boolean If validate is enabled and an error occours defined whether an exception should be thrown or not.
      * @return boolean Whether sending was sucessfull or not.
      */
-    public function send($text, $to, $from)
+    public function send($text, $to, $from, $validate = false, $throwException = false)
     {
+        if ($validate) {
+            $model = new DynamicModel(compact('to', 'from'));
+            $model->addRule(['from'], 'string', ['min' => 3, 'max' => 11]);
+            $model->addRule(['to'], PhoneNumberValidator::class, ['type' => PhoneNumberType::MOBILE]);
+
+            if (!$model->validate()) {
+                if ($throwException) {
+                    throw new InvalidParamException("The given parameters contain invalid values.");
+                }
+
+                return false;
+            }
+        }
+
         $response = $this->client->post(Resources::$SmsSend, ['body' => [
             'Text' => $text,
             'To' => $to,
